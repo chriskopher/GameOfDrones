5ac046238dfa338e92ee8fa1bcd129f1580d008e
Adan Moran <adanmoranm@gmail.com>
2014-11-12 15:54:21 -0500

    Update graph.m
    Started reading the data in the CSV file in row 2 if it is specified that there is a header.

--- src/graph.m
+++ src/graph.m
@@ -1,6 +1,15 @@
-function graph_m = graph(file)
+function graph_m = graph(file,header)
+    if ~exist('header')
+        header = false;
+    else
+        header = true;
+    end
     % Read the CSV file with the graph information into a matrix
     % Format is: "node1name,node1num,edgecost,edgetime,node2name,node2num"
-    data = csvread(file);
+    if header
+        data = csvread(file,2)
+    else
+        data = csvread(file)
+    end
 
 end
