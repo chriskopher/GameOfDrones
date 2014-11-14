68d07ed9d5e8565f39d1349f48f3904ba52557c1
Adan Moran-MacDonald <adanmoranm@gmail.com>
2014-11-14 13:24:33 -0800

    Added a function that reads multi-type csvs, and implemented it in graph. Graph is still not working, and I'm not sure what the error is yet.

--- src/graph.m
+++ src/graph.m
@@ -1,12 +1,16 @@
 function graph_m = graph(files)
     %Constants
-    HEADER_SIZE = 1;
-    
+    DELIM = ',';
+    class(files)
     % Read the CSV files with the graph information into a matrix
     % Format of names is: "node_num,node_name"
     % Format of edges is: "node1num,node2num,edge_type,edge_distance"
     % Format of weightings is: "edge_type,price_per_km,speed"
-    names = csvread(files{1},HEADER_SIZE);
-    edges = csvread(files{2},HEADER_SIZE);
-    weightings = csvread(files{3},HEADER_SIZE);
+    names = read_mixed_csv(files{1},DELIM);
+    fprintf('names ran\n');
+    edges = read_mixed_csv(files{2},DELIM);
+    fprintf('edges ran\n');
+    weightings = read_mixed_csv(files{3},DELIM);
+    fprintf('weightings ran\n');
+    graph_m = 0;
 end
