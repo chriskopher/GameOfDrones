65b5435021b3f15014702a73f239d25b0d9a18df
Adan Moran-MacDonald <adanmoranm@gmail.com>
2014-11-14 12:31:44 -0800

    Made the graph input a vector of filenames and updated the comments.

--- src/graph.m
+++ src/graph.m
@@ -1,15 +1,11 @@
-function graph_m = graph(file,header)
-    if ~exist('header')
-        header = false;
-    else
-        header = true;
-    end
-    % Read the CSV file with the graph information into a matrix
-    % Format is: "node1name,node1num,edgecost,edgetime,node2name,node2num"
-    if header
-        data = csvread(file,2)
-    else
-        data = csvread(file)
-    end
-
+function graph_m = graph(files)
+    % Read the CSV files with the graph information into a matrix
+    % Format of names is: "node_num,node_name"
+    % Format of edges is: "node1num,node2num,edge_type,edge_distance"
+    % Format of weightings is: "edge_type,price_per_km,speed"
+    names = csvread(files(1),2);
+    edges = csvread(files(2),2);
+    weightings = csvread(files(3),2);
+    
+    
 end
