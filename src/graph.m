b4244c0bb556803755c36b3b0756c022fd5f1dd7
Adan Moran-MacDonald <adanmoranm@gmail.com>
2014-11-14 12:59:50 -0800

    Added a testing file and updated the graph file to work with cells. However, it seems csvread won't work with the files we have because they are not numerical only. As such, we have to change the input function to create structs instead of matrices (not sure how to do that yet).

--- src/graph.m
+++ src/graph.m
@@ -1,11 +1,12 @@
 function graph_m = graph(files)
+    %Constants
+    HEADER_SIZE = 1;
+    
     % Read the CSV files with the graph information into a matrix
     % Format of names is: "node_num,node_name"
     % Format of edges is: "node1num,node2num,edge_type,edge_distance"
     % Format of weightings is: "edge_type,price_per_km,speed"
-    names = csvread(files(1),2);
-    edges = csvread(files(2),2);
-    weightings = csvread(files(3),2);
-    
-    
+    names = csvread(files{1},HEADER_SIZE);
+    edges = csvread(files{2},HEADER_SIZE);
+    weightings = csvread(files{3},HEADER_SIZE);
 end
