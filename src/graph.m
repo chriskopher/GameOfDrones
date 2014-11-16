ef43b6ba6fcf22cf25f34e081117e5a4717fa3db
Adan Moran-MacDonald <adanmoranm@gmail.com>
2014-11-16 12:17:26 -0800

    Changed graph.m to remove the headers from the matrices read from files, committing properly.

--- src/graph.m
+++ src/graph.m
@@ -4,23 +4,24 @@ function graph_m = graph(files,DEBUG)
     end
     %Constants
     DELIM = ',';
+    HEADERS = 1;
     
     % Read the CSV files with the graph information into a matrix
     % Format of names is: "node_num,node_name"
     % Format of edges is: "node1num,node2num,edge_type,edge_distance"
     % Format of weightings is: "edge_type,price_per_km,speed"
     names = read_mixed_csv(files{1},DELIM);
-    names = names(2:end,:);
+    names = names((HEADERS+1):end,:)
     if DEBUG
         fprintf('names ran:\n');
     end
     edges = read_mixed_csv(files{2},DELIM);
-    edges = edges(2:end,:);
+    edges = edges((HEADERS+1):end,:)
     if DEBUG
         fprintf('edges ran\n');
     end
     weightings = read_mixed_csv(files{3},DELIM);
-    weightings = weightings(2:end,:);
+    weightings = weightings((HEADERS+1):end,:)
     if DEBUG
         fprintf('weightings ran\n');
     end
