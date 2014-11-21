02ac408fd9d9b24f0e82fda97e72245d7e5a1ece
Adan Moran-MacDonald <adanmoranm@gmail.com>
2014-11-21 12:13:01 -0500

    Initialized the variables and added a loop to start searching the graph. Need to fill in what is done when the end node is not connected to the current node. This includes creating a backtracking method, creating a method to clean the memory matrix, creating a method to scan for the lowest cost in a vector, and the rest of the flowchart we've written down.

--- src/algorithm.m
+++ src/algorithm.m
@@ -1,3 +1,22 @@
-function path = algorithm(graph,start_node,end_node)
-
+function [path, total_weight] = algorithm(graph,start_node,end_node)
+% Initialize the global algorithm variables
+    current_node = start_node;
+    done = false;
+    memory_matrix = [start_node];
+    total_weight = 0;
+    
+% Start looping until we've arrived at the end node
+    while ~done
+       % Check to see if the end node is connected to the current node. If
+       % it is, go there and finish.
+       if graph(current_node,end_node) ~= -1
+           memory_matrix(1,end) = end_node;
+           done = true;
+       % If the end node is not connected to the current node...
+       else
+           
+       end
+    end
+    clean_matrix(memory_matrix);
+    path = memory_matrix;
 end
