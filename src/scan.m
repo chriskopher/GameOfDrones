d3327016e6d90dfd55e7703dc7a68fb3a3a4f8ed
Adan Moran-MacDonald <adanmoranm@gmail.com>
2014-11-21 13:00:20 -0500

    Filled in the scan function, which returns the column location of the smallest positive value in a 1xn vector.

--- src/scan.m
+++ src/scan.m
@@ -1,3 +1,15 @@
-function node = scan(vector)
-
+function column = scan(vector)
+% Function that scans for the lowest value in a horizontal vector that is
+% positive, and returns the column number of that value.
+    % Check if the given vector is a 1xn matrix.
+    if size(vector,1) > 1
+        throw(MException('SCAN:BadSize','Vector has too many rows.'));
+    end
+    
+    % Set all negative values to infinity, as we want to ignore them when we
+    % run the min function
+    vector(vector<0) = Inf;
+    
+    % Get the minimum value and set column to the column location.
+    [minval, column] = min(vector);
 end
