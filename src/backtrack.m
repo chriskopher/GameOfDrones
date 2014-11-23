c60fc00d5c479a248fdd7d471e10f71b84b02eaf
Adan Moran-MacDonald <adanmoranm@gmail.com>
2014-11-23 14:59:11 -0500

    Removed the C syntax and replaced it with MATLAB syntax.

--- src/backtrack.m
+++ src/backtrack.m
@@ -1,17 +1,12 @@
 function node = backtrack(current_node,memory_matrix)
-
-if (current_node == memory_matrix(1))
-    node = memory_matrix(2);
-else
-{
-  for i:size(memory_matrix)
-  {
-    if (i == current_node)
-    {
-      node = memory_matrix(i-1);
-      break;
-    }
+    if current_node == memory_matrix(1)
+        node = memory_matrix(2);
+    else
+        for i = 1:size(memory_matrix)
+          if i == current_node
+            node = memory_matrix(i-1);
+            break;
+          end
+        end
     end
-  }
-}
 end
